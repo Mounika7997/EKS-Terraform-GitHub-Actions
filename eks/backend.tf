3b943eb00a0855de3c4636bca24be6cb9ed391f5
MOUNIKA.M <92107249+Mounika7997@users.noreply.github.com>
2024-08-11 17:16:30 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -2,12 +2,22 @@ terraform {
   required_version = "~> 1.9.4"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "3.2.0"
+      source  = "aws"
+      version = "5.49.0"
     }
   }
 }
 
 provider "aws" {
   region  = var.aws-region
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "my-eks"
+    region         = "us-east-1"
+    key            = "eks/backend.tf"
+    dynamodb_table = "lock-id"
+    encrypt        = true
+  }
 }
